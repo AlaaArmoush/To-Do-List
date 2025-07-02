694bd8715689bf8a2cc47c4c1c6d2588fe1221da
Ala'a Armoush <armoush2004@gmail.com>
2025-07-02 17:41:47 +0300

    cleaner data storage

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+let tasks = [];
+
 function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
+    if(inputBox.value.trim() === ""){
+        alert("You have to write something first!");
+    }else{
+        tasks.push({
+            text: inputBox.value.trim(),
+            completed: false
+        });
+        inputBox.value = "";
+        saveData();
+        renderTasks();
     }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
 }
 
 inputBox.addEventListener("keypress", function(event){
@@ -23,25 +23,58 @@ inputBox.addEventListener("keypress", function(event){
     }
 });
 
+function loadTasks(){
+    const storedTasks = localStorage.getItem("tasks");
+    if(storedTasks){
+        tasks = JSON.parse(storedTasks);
+    }else{
+        tasks = [];
+    }
+}
+
+function renderTasks(){
+    listContainer.innerHTML = "";
+    let task, li;
+    for(let i = 0; i < tasks.length; i++){
+        task = tasks[i];
+        li = document.createElement("li");
+        li.textContent = task.text;
+
+        if(task.completed){
+            li.classList.add("checked");
+        }
+
+        li.setAttribute("data-index", i);
+        li.setAttribute("tabindex", "0");
+
+        span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+
+        listContainer.appendChild(li);
+    }
+}
+
+loadTasks();
+renderTasks();
+
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+        const index = e.target.getAttribute("data-index");
+        tasks[index].completed = !tasks[index].completed;
         saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
+        renderTasks();
+    } else if(e.target.tagName === "SPAN"){
+        const index = e.target.getAttribute("data-index");
+        tasks.splice(index, 1);
         saveData();
+        renderTasks();
     }
-
-}, false)
-
+});
 
 function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
 
-showTask();
+
